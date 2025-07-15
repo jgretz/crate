ae408c57074c96f969480e49c490557299acdfd0
Josh Gretz <jgretz@gmail.com>
2025-07-15 16:02:37 -0400

    file cleanup

--- apps/mobile/index.share.js
+++ apps/mobile/index.share.js
@@ -1,5 +1,5 @@
 import {AppRegistry} from 'react-native';
-import ShareExtension from './src/ShareExtension';
+import ShareExtension from './src/app/ShareExtension';
 
 // IMPORTANT: the first argument to registerComponent must be "shareExtension"
 AppRegistry.registerComponent('shareExtension', () => ShareExtension);
